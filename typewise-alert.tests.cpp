626fe84cbde45fc140218797e4ff2315b01a0a6c
UmangJ3 <haharofl14@gmail.com>
2024-08-23 13:51:36 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,7 +1,21 @@
 #include <gtest/gtest.h>
 #include "typewise-alert.h"
 
-TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
+TEST(TypeWiseAlertTestSuite, TestInferBreach) {
+    ASSERT_EQ(inferBreach(12.5, 20.0, 30.0), TOO_LOW);
+    ASSERT_EQ(inferBreach(25.0, 20.0, 30.0), NORMAL);
+    ASSERT_EQ(inferBreach(35.0, 20.0, 30.0), TOO_HIGH);
+}
 
+TEST(TypeWiseAlertTestSuite, TestClassifyTemperatureBreach) {
+    ASSERT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 25.0), NORMAL);
+    ASSERT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 36.0), TOO_HIGH);
+    ASSERT_EQ(classifyTemperatureBreach(HI_ACTIVE_COOLING, 46.0), TOO_HIGH);
+    ASSERT_EQ(classifyTemperatureBreach(MED_ACTIVE_COOLING, 39.0), NORMAL);
+}
 
+TEST(TypeWiseAlertTestSuite, TestCheckAndAlert) {
+    BatteryCharacter batteryChar = {PASSIVE_COOLING, "BrandX"};
+    checkAndAlert(TO_CONTROLLER, batteryChar, 20.0);  // Expecting NORMAL alert
+    checkAndAlert(TO_EMAIL, batteryChar, 36.0);       // Expecting TOO_HIGH alert
 }
